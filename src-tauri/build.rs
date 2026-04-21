05aab246bbb3982f2708b91dcaaea317525b2a82
Tirso Benedict Naza <benedictnaza@gmail.com>
2026-04-22 00:11:57 +0800

    chore: add rpath linker argument for swift libraries on macos in build script

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default() == "macos" {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,/usr/lib/swift");
+    }
     tauri_build::build();
 }
